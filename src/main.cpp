d7e3a592b784aeb19ab629643954628e29846b08
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-01-30 22:57:53 -0500

    Updated plugin info

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,10 @@
 
 // Mandatory plugin information.
 WUPS_PLUGIN_NAME("Nabbit");
-WUPS_PLUGIN_DESCRIPTION("Pretendo Network Patcher");
+WUPS_PLUGIN_DESCRIPTION("Pretendo Network Medals");
 WUPS_PLUGIN_VERSION("v0.1");
 WUPS_PLUGIN_AUTHOR("Pretendo contributors");
-WUPS_PLUGIN_LICENSE("ISC");
+WUPS_PLUGIN_LICENSE("AGPL3");
 
 // Gets called once the loader exists.
 INITIALIZE_PLUGIN() {

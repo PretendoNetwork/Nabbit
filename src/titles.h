517d5344c091ecc8b7e4809b77a043fec739d066
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-01-31 17:15:32 -0500

    Added title regions

--- src/titles.h
+++ src/titles.h
@@ -5,17 +5,28 @@
 #ifndef _PATCHER_H
 #define _PATCHER_H
 
+enum REGION {
+	JPN,
+	USA,
+	EUR
+};
+
 typedef void v();
 typedef struct Titles
 {
 	uint64_t title_id;
 	char name[80];
+	REGION region;
 	std::function<v> checkFunction;
 } Titles;
 
 static const Titles titles[] = {
-	{0x000500001010EC00, "MARIO KART 8", &checkMarioKart8Medals},
-	{0x0005000010176900, "Splatoon", &checkSplatoonMedals}
+	{0x0005000C1010EB00, "MARIO KART 8", JPN, &checkMarioKart8Medals},
+	{0x000500001010EC00, "MARIO KART 8", USA, &checkMarioKart8Medals},
+	{0x0005000C1010ED00, "MARIO KART 8", EUR, &checkMarioKart8Medals},
+	{0x0005000010162B00, "Splatoon", JPN, &checkSplatoonMedals},
+	{0x0005000010176900, "Splatoon", USA, &checkSplatoonMedals},
+	{0x0005000010176A00, "Splatoon", EUR, &checkSplatoonMedals}
 };
 
 #endif
